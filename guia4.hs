a5d7eec5079c0ffacb7f653edfbb58e2ce1d808f
Ignacio Gutman <108377975+nachogutman@users.noreply.github.com>
2025-04-14 16:13:56 -0300

    Start guide 4

---
+++ guia4.hs
@@ -0,0 +1,27 @@
+-- Ex 1
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n | n >= 2 = fibonacci (n-1) + fibonacci (n-2)
+
+-- Ex 2
+entirePart :: Float -> Integer
+entirePart n 
+    | n < 1 = 0
+    | otherwise = 1 + entirePart (n - 1)
+
+-- Ex 3
+isDivisible :: Integer -> Integer -> Bool
+isDivisible x y
+    | x == 0 = True
+    | x < 0 = False
+    | otherwise = isDivisible (x - y) y
+
+-- Ex 4 forma ez
+sumOddsWeird :: Integer -> Integer
+sumOddsWeird n = n*n
+
+-- Ex 4 recursive
+sumOdds :: Integer -> Integer
+sumOdds 1 = 1
+sumOdds n = 2*n - 1 + sumOdds(n - 1)
